936f1873acfd36b9bb06866b18d7a55a99f35cba
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-10-21 20:06:22 +0200

    Basic webpack configuation

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./assets/css/style.css";
+
+const title = document.createElement("h3");
+title.textContent = "Webpack made easy!";
+
+const page = document.querySelector("body");
+page.append(title);
